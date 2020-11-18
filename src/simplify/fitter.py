9030292731459c022b990bb3ca63659b1acf77bb
Eric Schanet <eric.schanet@cern.ch>
2020-11-18 08:46:01 +0100

    Black and minor changes to fit

--- src/simplify/fitter.py
+++ src/simplify/fitter.py
@@ -7,6 +7,7 @@ import pyhf
 from . import model_tools
 
 import logging
+
 log = logging.getLogger(__name__)
 
 
@@ -15,13 +16,15 @@ class FitResults(NamedTuple):
 
     bestfit: np.ndarray
     uncertainty: np.ndarray
-    labels: np.array
-    types: np.array
+    labels: List
+    types: List
     corr_mat: np.ndarray
     best_twice_nll: float
 
 
-def print_results(fit_result: FitResults, ) -> None:
+def print_results(
+    fit_result: FitResults,
+) -> None:
     """Prints best-fit parameter results and uncertainties.
 
     Parameters
@@ -37,7 +40,12 @@ def print_results(fit_result: FitResults, ) -> None:
         )
 
 
-def _fit_model_pyhf(model: pyhf.pdf.Model, data: List[float]) -> FitResults:
+def _fit_model_pyhf(
+    model: pyhf.pdf.Model,
+    data: List[float],
+    init_pars: Optional[List[float]] = None,
+    fixed_pars: Optional[List[bool]] = None,
+) -> FitResults:
     """Uses pyhf.infer API to perform a maximum likelihood fit.
 
     Parameters
@@ -46,6 +54,10 @@ def _fit_model_pyhf(model: pyhf.pdf.Model, data: List[float]) -> FitResults:
         Model to be used in the fit.
     data : List[float]
         Data to fit the model to.
+    init_pars : Optional[List[float]]
+        Initial parameter settings. Setting to none uses pyhf suggested ones.
+    fixed_pars : Optional[List[float]]
+        List of parameters to set to be fixed in the fit.
 
     Returns
     -------
@@ -56,13 +68,18 @@ def _fit_model_pyhf(model: pyhf.pdf.Model, data: List[float]) -> FitResults:
     pyhf.set_backend("numpy", pyhf.optimize.minuit_optimizer(verbose=True))
 
     result, result_obj = pyhf.infer.mle.fit(
-        data, model, return_uncertainties=True, return_result_obj=True
+        data,
+        model,
+        init_pars=init_pars,
+        fixed_params=fixed_pars,
+        return_uncertainties=True,
+        return_result_obj=True,
     )
 
     bestfit = result[:, 0]
     uncertainty = result[:, 1]
-    labels = np.asarray(model_tools.get_parameter_names(model))
-    types = np.asarray(model_tools.get_parameter_types(model))
+    labels = model_tools.get_parameter_names(model)
+    types = model_tools.get_parameter_types(model)
     corr_mat = result_obj.minuit.np_matrix(correlation=True, skip_fixed=False)
     best_twice_nll = float(result_obj.fun)
 
@@ -105,8 +122,6 @@ def fit(spec: Dict[str, Any], asimov: bool = False) -> FitResults:
     fit_result = _fit_model_pyhf(model, data)
 
     # log.debug(print_results(fit_result))
-    log.debug(
-        f"-2 log(L) = {fit_result.best_twice_nll:.6f} at the best-fit point"
-    )
+    log.debug(f"-2 log(L) = {fit_result.best_twice_nll:.6f} at the best-fit point")
 
     return fit_result
