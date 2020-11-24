46a64d7a8a38178732fc850398a784478383ef88
Eric Schanet <eric.schanet@cern.ch>
2020-11-24 08:46:42 +0100

    tests

--- tests/test_plot.py
+++ tests/test_plot.py
@@ -144,3 +144,101 @@ def test_data_MC(
     assert np.allclose(mock_draw.call_args_list[1][0][2], np.asarray([1, 2]))
     assert mock_draw.call_args_list[1][0][3] == pathlib.Path("tmp/SR_postfit.pdf")
     assert mock_draw.call_args_list[1][1] == {"log_scale": False}
+
+
+@mock.patch("simplify.helpers.plotting.correlation_matrix")
+def test_correlation_matrix(mock_draw):
+    corr_mat = np.asarray([[1.0, 0.2, 0.1], [0.2, 1.0, 0.1], [0.1, 0.1, 1.0]])
+    corr_mat_pruned = np.asarray([[1.0, 0.2], [0.2, 1.0]])
+    labels = ["a", "b", "c"]
+    labels_pruned = ["a", "b"]
+    types = ["constrained", "constrained", "constrained"]
+    folder_path = "tmp"
+    figure_path = pathlib.Path(folder_path) / "correlation_matrix.pdf"
+    fit_results = fitter.FitResults(
+        np.empty(0), np.empty(0), labels, types, corr_mat, 1.0
+    )
+
+    # pruning with threshold
+    plot.correlation_matrix(
+        fit_results, output_path=pathlib.Path(folder_path), pruning_threshold=0.15
+    )
+
+    mock_draw.assert_called_once()
+    assert np.allclose(mock_draw.call_args[0][0], corr_mat_pruned)
+    assert np.any(
+        [mock_draw.call_args[0][1][i] == labels[i] for i in range(len(labels_pruned))]
+    )
+    assert mock_draw.call_args[0][2] == figure_path
+    assert mock_draw.call_args[1] == {}
+
+    # pruning of fixed parameter (all zeros in correlation matrix row/column)
+    corr_mat_fixed = np.asarray([[1.0, 0.2, 0.0], [0.2, 1.0, 0.0], [0.0, 0.0, 0.0]])
+    fit_results_fixed = fitter.FitResults(
+        np.empty(0), np.empty(0), labels, types, corr_mat_fixed, 1.0
+    )
+    plot.correlation_matrix(fit_results_fixed, output_path=pathlib.Path(folder_path))
+    assert np.allclose(mock_draw.call_args_list[1][0][0], corr_mat_pruned)
+    assert np.any(
+        [
+            mock_draw.call_args_list[1][0][1][i] == labels[i]
+            for i in range(len(labels_pruned))
+        ]
+    )
+
+
+@mock.patch("simplify.helpers.plotting.pulls")
+def test_pulls(mock_draw):
+    bestfit = np.asarray([0.8, 1.0, 1.05, 1.1])
+    uncertainty = np.asarray([0.9, 1.0, 0.03, 0.7])
+    labels = ["a", "b", "staterror_region[bin_0]", "c"]
+    types = ["constrained", "constrained", "unconstrained", "constrained"]
+    exclude_list = ["a"]
+    folder_path = "tmp"
+    fit_results = fitter.FitResults(
+        bestfit, uncertainty, labels, types, np.empty(0), 1.0
+    )
+
+    filtered_bestfit = np.asarray([1.0, 1.1])
+    filtered_uncertainty = np.asarray([1.0, 0.7])
+    filtered_labels = np.asarray(["b", "c"])
+    figure_path = pathlib.Path(folder_path) / "pulls.pdf"
+
+    # with filtering
+    plot.pulls(
+        fit_results,
+        output_path=pathlib.Path(folder_path),
+        exclude_list=exclude_list,
+    )
+
+    mock_draw.assert_called_once()
+    assert np.allclose(mock_draw.call_args[0][0], filtered_bestfit)
+    assert np.allclose(mock_draw.call_args[0][1], filtered_uncertainty)
+    assert np.any(
+        [
+            mock_draw.call_args[0][2][i] == filtered_labels[i]
+            for i in range(len(filtered_labels))
+        ]
+    )
+    assert mock_draw.call_args[0][6] == figure_path
+    assert mock_draw.call_args[1] == {}
+
+    # without filtering via list, but with staterror removal
+    # and fixed parameter removal
+    fit_results.uncertainty[0] = 0.0
+
+    bestfit_expected = np.asarray([1.0, 1.1])
+    uncertainty_expected = np.asarray([1.0, 0.7])
+    labels_expected = ["b", "c"]
+    plot.pulls(fit_results, output_path=folder_path)
+
+    assert np.allclose(mock_draw.call_args[0][0], bestfit_expected)
+    assert np.allclose(mock_draw.call_args[0][1], uncertainty_expected)
+    assert np.any(
+        [
+            mock_draw.call_args[0][2][i] == labels_expected[i]
+            for i in range(len(labels_expected))
+        ]
+    )
+    assert mock_draw.call_args[0][6] == figure_path
+    assert mock_draw.call_args[1] == {}
