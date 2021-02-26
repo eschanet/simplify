81e4e7ec8083eb32a94cd9f876c42a5ff00e34e7
Eric Schanet <eric.schanet@cern.ch>
2021-02-26 17:17:38 +0100

    Fix: CI should work again

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ extras_require["test"] = sorted(
             "flake8-import-order",
             "flake8-print",
             "mypy",
-            "typeguard",
+            "typeguard~=2.10.0",
             "pydocstyle",
             "black",
         ]
